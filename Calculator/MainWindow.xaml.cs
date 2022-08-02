00fde16b8456e406b02605115bb806ab20af155c
Charlotte-R <49107073+Charlotte-R-01134@users.noreply.github.com>
2022-08-02 15:22:35 +0100

    functional calculator

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -22,9 +22,8 @@ namespace Calculator
     {
         #region Private Members
 
-        private Type[] results;
-        private bool pressedEnter;
-        private bool pressedClear;
+        private bool max;
+
 
         #endregion
 
@@ -34,15 +33,16 @@ namespace Calculator
             InitializeComponent();
 
             NewCalc();
+
         }
 
         #endregion
 
         private void NewCalc()
         {
-            results = new Type[10];
-            pressedEnter = false;
-            pressedClear = false;
+            max = false;
+
+            box.Text = "";
 
         }
 
@@ -51,96 +51,162 @@ namespace Calculator
         /// </summary>
         private void Button_Clear_Click(object sender, RoutedEventArgs e)
         {
-
+            NewCalc();
         }
 
-        private void Button_LBracket_Click(object sender, RoutedEventArgs e)
+        private void Button_Square_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!max)
+            {
+                box.Text += "^";
+                max = true;
+            }
         }
 
-        private void Button_RBracket_Click(object sender, RoutedEventArgs e)
+        private void Button_Square_Root_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!max)
+            {
+                box.Text += "√";
+                max = true;
+            }
         }
 
         private void Button_Add_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!max)
+            {
+                box.Text += "+";
+                max = true;
+            }
         }
 
         private void Button_7_Click(object sender, RoutedEventArgs e)
         {
-
+            box.Text += "7";
         }
 
         private void Button_8_Click(object sender, RoutedEventArgs e)
         {
-
+            box.Text += "8";
         }
 
         private void Button_9_Click(object sender, RoutedEventArgs e)
         {
-
+            box.Text += "9";
         }
 
         private void Button_Minus_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!max)
+            {
+                box.Text += "-";
+                max = true;
+            }
         }
 
         private void Button_4_Click(object sender, RoutedEventArgs e)
         {
-
+            box.Text += "4";
         }
 
         private void Button_5_Click(object sender, RoutedEventArgs e)
         {
-
+            box.Text += "5";
         }
 
         private void Button_6_Click(object sender, RoutedEventArgs e)
         {
-
+            box.Text += "6";
         }
 
         private void Button_Multiplication_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!max)
+            {
+                box.Text += "x";
+                max = true;
+            }
         }
 
         private void Button_1_Click(object sender, RoutedEventArgs e)
         {
-
+            box.Text += "1";
         }
 
         private void Button_2_Click(object sender, RoutedEventArgs e)
         {
-
+            box.Text += "2";
         }
 
         private void Button_3_Click(object sender, RoutedEventArgs e)
         {
-
+            box.Text += "3";
         }
 
         private void Button_Divide_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!max)
+            {
+                box.Text += "÷";
+                max = true;
+            }
         }
 
         private void Button_Decimal_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!max)
+            {
+                box.Text += ".";
+                max = true;
+            }
         }
 
         private void Button_0_Click(object sender, RoutedEventArgs e)
         {
-
+            box.Text += "0";
         }
+
 
         private void Button_Enter_Click(object sender, RoutedEventArgs e)
         {
+            var contents = box.Text;
+            if (contents.Contains("+"))
+            {
+                var results = contents.Split('+');
+                box.Text = (Int32.Parse(results[0]) + Int32.Parse(results[1])).ToString();
+                max = false;
+            }
+            if (contents.Contains("-"))
+            {
+                var results = contents.Split('-');
+                box.Text = (Int32.Parse(results[0]) - Int32.Parse(results[1])).ToString();
+                max = false;
+            }
+            if (contents.Contains("x"))
+            {
+                var results = contents.Split('x');
+                box.Text = (Int32.Parse(results[0]) * Int32.Parse(results[1])).ToString();
+                max = false;
+            }
+            if (contents.Contains("÷"))
+            {
+                var results = contents.Split('÷');
+                box.Text = (Int32.Parse(results[0]) / Int32.Parse(results[1])).ToString();
+                max = false;
+            }
+            if (contents.Contains("^"))
+            {
+                var results = contents.Split('^');
+                box.Text = Math.Pow(Int32.Parse(results[0]), Int32.Parse(results[1])).ToString();
+                max = false;
+            }
+            if (contents.Contains("√"))
+            {
+                var results = contents.Substring(1);
+                box.Text = Math.Sqrt(Convert.ToDouble(Int32.Parse(results))).ToString();
+                max = false;
+            }
 
         }
     }
